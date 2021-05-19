69940938635dcce3e23e0b3df1e842992aeac303
Arthur Wasenmueller <a.wasenmueller@gmail.com>
2021-05-19 15:34:59 +0200

    seeding completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Ushido = { name: "Ushido", address: "Lychener Str. 18, 10437 Berlin", category: "japanese" }
+Gazzo =  { name: "Gazzo", address: "Hobrechtstraße 57, 12047 Berlin", category: "italian" }
+Zola =  { name: "Zola", address: "Paul-Lincke-Ufer 39-40, 10999 Berlin", category: "italian" }
+Feedback = { name: "Feedback", address: "Paul-Lincke-Ufer 45, 10999 Berlin", category: "chinese" }
+Lamazère = { name: "Feedback", address: "Stuttgarter Platz 18, 10627 Berlin", category: "french" }
+
+
+[ Ushido, Gazzo, Zola, Feedback, Lamazère ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
